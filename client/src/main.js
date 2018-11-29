e741569aafcf8db52ce422e08cd2f0f911f7a420
Eddie Franco <eddiefranco626@gmail.com>
2018-11-29 15:47:30 -0600

    Working bootstrap

--- client/src/main.js
+++ client/src/main.js
@@ -3,9 +3,13 @@ import App from './App.vue'
 import router from './router'
 import store from './store'
 import axios from 'axios'
+import BootstrapVue from 'bootstrap-vue'
+import 'bootstrap/dist/css/bootstrap.css'
+import 'bootstrap-vue/dist/bootstrap-vue.css'
 
 Vue.prototype.$api = axios
 
+Vue.use(BootstrapVue)
 Vue.config.productionTip = false
 
 new Vue({
