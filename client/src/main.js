41eb617be5f11ef37cceca23b3887f4832c75e4d
John Caldwell <jmcald0@gmail.com>
2018-12-01 13:34:52 -0600

    revert

--- client/src/main.js
+++ client/src/main.js
@@ -1,11 +1,12 @@
 import Vue from 'vue'
-import App from './App.vue'
 import router from './router'
 import store from './store'
 import axios from 'axios'
 import BootstrapVue from 'bootstrap-vue'
 import 'bootstrap/dist/css/bootstrap.css'
 import 'bootstrap-vue/dist/bootstrap-vue.css'
+import 'mdbvue/build/css/mdb.css'
+import App from './App.vue'
 
 Vue.prototype.$api = axios
 
