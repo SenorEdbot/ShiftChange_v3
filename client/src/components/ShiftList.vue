1b6dcb2209b181f8925ca9367733036a719ae909
justinkane88@yahoo.com <justinkane88@yahoo.com>
2018-12-01 12:12:42 -0600

    change shift list and format date w/ moment. added dependencies. npm install momment now required

--- client/src/components/ShiftList.vue
+++ client/src/components/ShiftList.vue
@@ -2,92 +2,92 @@
   <div class="row">
     <div class="col-8 offset-2">
       <h3>Current Shifts:</h3>
-      <!-- <ul id="shift-list" class="list-group">
-        <li v-for="shift in shifts" :key="shift.id">
-          {{shift.name}} | 
-          {{shift.reason}} |
-          {{shift.date}} |
-          {{shift.duration}} |
-          {{shift.role}} |
-          {{shift.covered}} |
-          {{shift.priority}} |
-          {{shift.personResponsible}}
-          <button @click="deleteShift(shift.id)" class="btn btn-danger float-right delete">x</button>
-        </li>
-      </ul> -->
-      <table class="table table-responsive">
-          <thead>
+
+<accordion :panes="panes"/>
+    </div>
+  </div>
+</div>
+</template>
+
+<script>
+import shiftService from "@/services/shiftService";
+import { Accordion } from 'mdbvue';
+var moment = require('moment');
+moment().format();
+
+export default {
+  name: "ShiftList",
+  data: () => ({
+    shifts: [],
+    panes: []
+  }),
+  created() {
+    this.updateShifts();
+  },
+  methods: {
+    updateShifts() {
+      shiftService.all().then(res => {
+        this.shifts = res.data;
+      for (let i = 0; i < this.shifts.length; i++){
+        // })
+        let pane = {}
+        pane.title = `${res.data[i].name} 
+                      <br>
+                      ${moment(res.data[i].date,'YYYY-MM-DD hh:mm:ss').format('MM-DD-YYYY')}`
+        pane.content = `
+        <table class="table table-responsive">
+        <thead>
             <tr>
               <th>ID</th>
-              <th>Name</th>
               <th>Reason</th>
-              <th>Date</th>
               <th>Duration</th>
               <th>Role</th>
               <th>Covered</th>
               <th>Priority</th>
               <th>Person Responsible</th>
-              <th>Claim</th>
-              <th>Edit</th>
-              <th>Delete</th>
             </tr>
           </thead>
           <tbody>
             <tr v-for="shift in shifts" :key="shift.id">
-              <td>{{ shift.id }}</td>
-              <td>{{ shift.name }}</td>
-              <td>{{ shift.reason }}</td>
-              <td>{{ shift.date }}</td>
-              <td>{{ shift.duration }}</td>
-              <td>{{ shift.role }}</td>
-              <td>{{ shift.covered }}</td>
-              <td>{{ shift.priority }}</td>
-              <td>{{ shift.personResponsible }}</td>
-              <td>
-                <b-btn>Claim</b-btn>
-              </td>
-              <td>
-                <b-btn>Edit</b-btn>
-              </td>
-              <td>
-                <b-btn @click="deleteShift(shift.id)" class="btn btn-success float-right delete">x</b-btn>
-              </td>
+              <td>${res.data[i].id}</td>
+              <td>${res.data[i].reason}</td>
+              <td>${res.data[i].duration}</td>
+              <td>${res.data[i].role}</td>
+              <td>${res.data[i].covered}</td>
+              <td>${res.data[i].priority}</td>
+              <td>${res.data[i].personResponsible}</td>
+              </tr>
+              <tr>
+            
             </tr>
-          </tbody>
-        </table>
-    </div>
-  </div>
-</template>
-
-<script>
-import shiftService from '@/services/shiftService'
-
-export default {
-  name: 'ShiftList',
-  data: () => ({
-    shifts: []
-  }),
-  created () {
-    this.updateShifts()
-  },
-  methods: {
-    updateShifts () {
-      shiftService.all().then((res) => {
-        this.shifts = res.data
-      })
+            </table>
+            <table class="table table-responsive">
+              <ul class="list-inline" style="list-style-type: none;">
+                <li class="list-inline-item"><b-btn class="btn btn-success">Claim</b-btn
+                <li class="list-inline-item"><b-btn class="btn btn-success">Edit</b-btn>
+                <li class="list-inline-item"><b-btn @click="deleteShift(shift.id)" class="btn btn-danger delete">x</b-btn>
+              </ul>
+              </table>
+         `
+        this.panes.push(pane)
+      }
+      });
     },
-    deleteShift (id) {
-      shiftService.delete(id).then((res) => {
-        this.updateShifts()
-      })
+    deleteShift(id) {
+      shiftService.delete(id).then(res => {
+        this.updateShifts();
+      });
     }
+  },
+  components: {
+    Accordion,
   }
-}
+};
 </script>
 
 <style scoped>
-h3{
-  color: #42b983;;
+h3 {
+  color: #42b983;
 }
 th {
   font-size: 18px;
@@ -95,6 +95,6 @@ th {
 tbody {
   background-image: linear-gradient(rgb(207, 252, 201), #42b983);
   box-shadow: 0 0 40px black;
-  
 }
+
 </style>
